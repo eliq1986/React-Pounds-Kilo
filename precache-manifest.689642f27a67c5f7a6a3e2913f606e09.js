b41e146f0244afc4c79cd66ea385b53c319876f9
eliq1986 <elijahmquesada@gmail>
2019-02-10 10:46:32 -0800

    Updates

---
+++ precache-manifest.689642f27a67c5f7a6a3e2913f606e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fac35c7b89ab50a39ec",
+    "url": "/React-Pounds-Kilo/static/js/runtime~main.1fac35c7.js"
+  },
+  {
+    "revision": "9959c98e73eef112e623",
+    "url": "/React-Pounds-Kilo/static/js/main.9959c98e.chunk.js"
+  },
+  {
+    "revision": "bcc325a827a623913b93",
+    "url": "/React-Pounds-Kilo/static/js/1.bcc325a8.chunk.js"
+  },
+  {
+    "revision": "9959c98e73eef112e623",
+    "url": "/React-Pounds-Kilo/static/css/main.9b0a678d.chunk.css"
+  },
+  {
+    "revision": "bcc325a827a623913b93",
+    "url": "/React-Pounds-Kilo/static/css/1.e33fd8f2.chunk.css"
+  },
+  {
+    "revision": "ea3cab689074188c502c04942a07e4e0",
+    "url": "/React-Pounds-Kilo/index.html"
+  }
+];
